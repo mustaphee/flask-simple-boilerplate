9bb8fd8110761fe2ef7f6c63721f38307202df35
Yusuff Mustapha <mustaphee94@gmail.com>
2019-04-21 18:35:19 +0100

    App entry point locally

--- runserver.py
+++ runserver.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
 from app.app import create_app, register_extensions
 from app.config.config import DevelopmentConfig
 
+# Create an app instance and run
 if __name__ == '__main__':
     app = create_app(DevelopmentConfig)
     register_extensions(app)
-    PORT = int(app.config['PORT'])
+    PORT = int(app.config['PORT']) or 2000
     app.run(port=PORT)
