dcf6d50d816ec2cd79331faa99a88170ea208882
Yusuff Mustapha <mustaphee94@gmail.com>
2019-04-21 21:33:29 +0100

    configurations file for the app

---
+++ app/config/config.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+class Config(object):
+    """Config base class
+    This is the default configuration class for your app. You should
+    `probably` not use this directly, instead extend this class with
+     your custom config for different environments
+    """
+    DEBUG = False
+    TESTING = False
+    ENV = 'development'
+
+
+class DevelopmentConfig(Config):
+    """Development Config
+    This extends the `Config` base class to store variables
+    for development environments
+    """
+    DEBUG = True
+    FLASK_APP = 'APP-DEV'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_DEV')
+    PORT = os.getenv('PORT')
+
+class TestingConfig(Config):
+    """Testing Config
+       This extends the `Config` base class to store variables
+       for testing environments
+       """
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_TESTING')
+
+
+class StagingConfig(Config):
+    """Staging Config
+       This extends the `Config` base class to store variables
+       for staging environments
+
+       """
+    pass
+
+
+class ProductionConfig(Config):
+    """Production Config
+       This extends the `Config` base class to store variables
+       for production environments
+
+       """
+    ENV = 'production'
+
